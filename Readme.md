e797c90961000192cbcf1d28dd7666583112503c
iɴᴜʟʟɴᴇᴛ <12373106+iNullNet@users.noreply.github.com>
2020-07-22 12:55:23 +0100

    commit

---
+++ Readme.md
@@ -0,0 +1,14 @@
+## README
+
+Simple script for check exchange-rate by  [**Transferwise**](https://transferwise.com/) .
+
+You need only use chmod +x to exchange-rate.sh file and execute that for see exchange rate.
+
+Example: if you want to check 
+
+- £ = $ (British POUND with Dollar U.S.A) use "exchange-rate.sh GBP USD" 
+- € = £ (EURO with British POUND) use "exchange-rate.sh EUR GBP"
+
+that's it.
+If you want you can also put in /usr/local/bin for exec everywhere (path) where you are.
+
---
+++ exchange-rate.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+### VARIABLE ###
+source_to_find="$1"
+target_to_find="$2"
+primo=0
+secondo=0
+source_to_find=$(echo "$1" |awk '{print tolower($0)}') #4 works need lowercase
+target_to_find=$(echo "$2" |awk '{print tolower($0)}') #4 works need lowercase
+
+function VARIABLE_CHECK() {
+	printf "\n"
+	printf "\n"
+	if [ -z "$source_to_find" ]; then
+		echo "source variable is not set"
+		primo=1
+	fi
+	if [ -z "$target_to_find" ]; then
+		echo "target variable is not set"
+		secondo=1
+		printf "\n"
+	fi
+	if [ "$primo" == 1 ] || [ "$secondo" == 1 ]; then
+		echo "need to use => command "'$1'" "'$2'" <="
+		echo "example => command GBP EUR"
+		printf "\n"
+		exit
+	fi
+}
+
+function RECOVER() {
+	echo -e "loading in progress.."
+	rate=$(curl -s https://transferwise.com/us/currency-converter/$source_to_find-to-$target_to_find-rate |grep "currentRate" |tail -n 1 |cut -d: -f2 |tr -d '",' |awk '{print $3}')
+	source=$(curl -s https://transferwise.com/us/currency-converter/$source_to_find-to-$target_to_find-rate |grep "source" |tail -n 1  |cut -d: -f2 |tr -d '",' |awk '{print $3}')
+	target=$(curl -s https://transferwise.com/us/currency-converter/$source_to_find-to-$target_to_find-rate |grep "target" |tail -n 1 |cut -d: -f2 |tr -d '",' |awk '{print $3}')
+	echo -e "done!!"
+}
+
+function SHOW() {
+	printf "\n"
+	#printf "\n"
+	#echo -e "source => $source"
+	#echo -e "rate   => $rate"
+	#echo -e "target => $target"
+	#printf "\n"
+	echo -e "1 $source = $rate $target"
+	printf "\n"
+}
+
+
+
+### MAIN ###
+VARIABLE_CHECK
+RECOVER
+SHOW
